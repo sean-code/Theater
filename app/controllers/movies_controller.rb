d60fa3e69ef092f45f84da050b8b1b85dfc2213f
sean-code <john.nganga@student.moringaschool.com>
2023-01-01 14:03:36 +0300

    create index and show actions for displaying all movies and movies by id

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,12 @@
 class MoviesController < ApplicationController
+    def index
+        @movies=Movie.all
+        render json: @movies, except: [:created_at, :updated_at], status: :ok
+    end
+
+    def show
+        @movie=Movie.find_by(id: params[:id])
+        render json: @movie, except: [:created_at, :updated_at], status: :ok
+    end
+
 end
