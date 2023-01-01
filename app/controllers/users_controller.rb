e261b54ef22ef53ca067cfe07b879be3066abe7b
sean-code <john.nganga@student.moringaschool.com>
2023-01-01 14:18:58 +0300

    Create index and show actions for all users and user by id respectively

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+   
+    #Display all users
+    def index
+        @users=User.all
+        render json: @users, except: [:created_at, :updated_at], status: :ok
+    end
+
+    def show
+        @user=User.find_by(id: params[:id])
+        render json: @user, except: [:created_at, :updated_at], status: :ok
+    end
 end
